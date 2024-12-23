a8647f1cb0b972159207015beae71b6c61147342
Mavludin <imevlud@gmail.com>
2024-12-23 13:20:11 +0300

    filter storage json by fields

--- android/app/src/main/java/com/test/CustomBroadcastReceiver.kt
+++ android/app/src/main/java/com/test/CustomBroadcastReceiver.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.os.Bundle
 import android.os.Build
 import android.content.Intent
+import org.json.JSONObject
 
 import android.database.sqlite.SQLiteDatabase
 
@@ -20,40 +21,41 @@ class CustomBroadcastReceiver: BroadcastReceiver() {
     if (action == "com.test.SEND_DATA_REQUEST") {
       Log.d("CustomBroadcastReceiver", "Received broadcast with action: $action and data: $data")
 
-      // Чтение данных из базы данных SQLite (AsyncStorage)
-      val storedValue = readFromAsyncStorage(context)
+      val json = getDataFromAsyncStorage(context)
 
-      // Логирование прочитанных данных
-      Log.d("CustomBroadcastReceiver", "Data from AsyncStorage: $storedValue")
+      Log.d("CustomBroadcastReceiver", "Data from AsyncStorage: $json")
+
+      val fields = listOf("test", "income")
+      val filteredJSON = filterJSONByFields(json, fields)
+
+      Log.d("CustomBroadcastReceiver", "Filtered data from AsyncStorage: $filteredJSON")
 
       val senderAction = "com.nextshift.SEND_DATA"
 
       // Отправка данных обратно через broadcast
       val intent = Intent(senderAction).apply {
-        putExtra("data", storedValue)
+        putExtra("data", filteredJSON)
         setPackage("com.nextshift")
       }
 
-      context.sendBroadcast(intent) // Отправляем Broadcast
+      context.sendBroadcast(intent)
 
       Log.d("CustomBroadcastReceiver", "Broadcast response sent")
     }
   }
 
-  private fun readFromAsyncStorage(context: Context): String? {
+  private fun getDataFromAsyncStorage(context: Context): String? {
     val databasePath = context.getDatabasePath("RKStorage").absolutePath
     val database: SQLiteDatabase
 
     try {
-      // Открываем базу данных SQLite
       database = SQLiteDatabase.openDatabase(databasePath, null, SQLiteDatabase.OPEN_READONLY)
 
-      // Выполняем запрос для получения значения по ключу
       val cursor = database.rawQuery("SELECT value FROM catalystLocalStorage WHERE key = ?", arrayOf("persist:root"))
       var value: String? = null
 
       if (cursor.moveToFirst()) {
-        value = cursor.getString(0) // Получаем значение из столбца "value"
+        value = cursor.getString(0)
       }
 
       cursor.close()
@@ -62,7 +64,27 @@ class CustomBroadcastReceiver: BroadcastReceiver() {
       return value
     } catch (e: Exception) {
       Log.e("AsyncStorageReader", "Error reading from AsyncStorage database", e)
+
       return null
     }
   }
+
+  fun filterJSONByFields(json: String?, fields: List<String>): String? {
+    return try {
+      val jsonObject = JSONObject(json)
+      val filteredObject = JSONObject()
+
+      for (field in fields) {
+        if (jsonObject.has(field)) {
+          filteredObject.put(field, jsonObject.get(field))
+        }
+      }
+
+      filteredObject.toString()
+    } catch (e: Exception) {
+      e.printStackTrace()
+      null
+    }
+  }
+
 }
