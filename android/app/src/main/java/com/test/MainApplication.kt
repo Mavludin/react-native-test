942698ec5d1fb6e519f29ff6481e33cabe4fb00e
Mavludin <imevlud@gmail.com>
2024-12-25 16:35:57 +0300

    remove BroadcastSender package

--- android/app/src/main/java/com/test/MainApplication.kt
+++ android/app/src/main/java/com/test/MainApplication.kt
@@ -12,8 +12,6 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
-import com.test.BroadcastSenderPackage
-
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
@@ -21,7 +19,6 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              add(BroadcastSenderPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
