6d0e7a8650571cb82bd399a726a82f84475880c0
Mavludin <imevlud@gmail.com>
2024-12-25 16:41:52 +0300

    add settings.kt

---
+++ android/app/src/main/java/com/test/settings.kt
@@ -0,0 +1,8 @@
+package com.test
+
+object Settings {
+  var sendDataRequestAction = "com.test.SEND_DATA_REQUEST"
+  var sendDataAction = "com.nextshift.SEND_DATA"
+  var nextshiftPackage = "com.nextshift"
+  var allowedFields = listOf("workShift", "userShifts", "settings")
+}
