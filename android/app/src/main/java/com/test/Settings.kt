2f117d2d32644bce8b9a477b887262a01bef1bc2
Mavludin <imevlud@gmail.com>
2024-12-30 19:35:30 +0300

    add notes allowedFields

--- android/app/src/main/java/com/test/Settings.kt
+++ android/app/src/main/java/com/test/Settings.kt
@@ -4,5 +4,5 @@ object Settings {
   var sendDataRequestAction = "com.test.SEND_DATA_REQUEST"
   var sendDataAction = "com.nextshift.SEND_DATA"
   var nextshiftPackage = "com.nextshift"
-  var allowedFields = listOf("workShift", "userShifts", "settings")
+  var allowedFields = listOf("workShift", "userShifts", "settings", "notes")
 }
