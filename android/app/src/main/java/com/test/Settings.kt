7a5cd3864b48519af6acd3c9ceece487d4e00a7b
Mavludin <imevlud@gmail.com>
2025-02-17 21:48:38 +0300

    replace com.nextshift with com.shiftworkerslab.nextshift

--- android/app/src/main/java/com/test/Settings.kt
+++ android/app/src/main/java/com/test/Settings.kt
@@ -2,7 +2,7 @@ package com.test
 
 object Settings {
   var sendDataRequestAction = "com.test.SEND_DATA_REQUEST"
-  var sendDataAction = "com.nextshift.SEND_DATA"
-  var nextshiftPackage = "com.nextshift"
+  var sendDataAction = "com.shiftworkerslab.nextshift.SEND_DATA"
+  var nextshiftPackage = "com.shiftworkerslab.nextshift"
   var allowedFields = listOf("workShift", "userShifts", "settings", "notes", "alarms")
 }
